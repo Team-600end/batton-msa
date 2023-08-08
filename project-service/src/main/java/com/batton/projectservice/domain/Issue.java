2e8dcd29ae43a2add746cc73daeb9d14183908a1
chkang13 <chkang31@naver.com>
2023-08-08 10:59:31 +0900

    BS-40/[BS-82] 이슈 레포트 조회 api 변경

--- project-service/src/main/java/com/batton/projectservice/domain/Issue.java
+++ project-service/src/main/java/com/batton/projectservice/domain/Issue.java
@@ -30,7 +30,7 @@ public class Issue extends BaseEntity implements Comparable<Issue> {
     private Project project;
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "belong_id")
-    private Belong belong;;
+    private Belong belong;
 
     @Builder
     public Issue(Long id, String issueTitle, String issueContent, IssueStatus issueStatus, IssueTag issueTag, int issueSeq, int issueKey, Project project, Belong belong) {
